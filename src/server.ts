a169dd44bd285f8d2e0728f67912b4df32724055
ksh <kangshqwert@naver.com>
2023-03-23 17:20:57 +0900

    [기능추가]: 라우트 추가, 세션 추가

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,28 @@
 import express, { Application } from 'express';
+import session from 'express-session';
+
+import routes from './routes';
+import sequelize from './sequelize';
+
+const FileStore = require('session-file-store')(session);
 
 const app: Application = express();
+
+const sessionMiddleware = session({
+  secret: 'cacaonibs',
+  saveUninitialized: true,
+  cookie: { secure: false },
+  resave: false,
+  store: new FileStore(),
+});
+
+app.use(sessionMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: true });
+
+app.use('/', routes);
 
 app.listen(8000, () => {
   console.log('start');
